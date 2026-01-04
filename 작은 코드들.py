519fdbd6d113d497504ec1da1785d05f8251d9bb
kwon <nayeong0918@gmail.com>
2026-01-04 22:33:33 +0900

    Add basic variable and loop examples in Python

---
+++ 작은 코드들.py
@@ -0,0 +1,27 @@
+# 변수 생성
+a = ('haroha ')
+s = ('권kwon ')
+q = ('isegye ')
+
+# 변수 출력 반복문 
+for i in range(3):
+    print(a)
+
+for i in range(3):
+    print(s)
+
+for i in range(3):
+    print(q)
+
+# 문자열 출력 반복문
+for i in range(3):
+    print('1')
+    print('0')
+
+# 숫자 출력 반복문
+for i in range(6):
+    print(i)
+
+# 변수 출력
+print(a+s+q)
+
